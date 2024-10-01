68f94bc85b111798897639f6ae4b087b3e22029b
Jonathan Conway <jon@conwy.co>
2024-10-01 20:42:29 +1000

    feat(article-ai-patterns): update from linkedin

--- src/content/articles/ai-patterns/meta.ts
+++ src/content/articles/ai-patterns/meta.ts
@@ -1,9 +1,9 @@
-import { ArticleMeta } from "@/framework/client";
+import { ArticleMeta, SocialLinkTypes } from "@/framework/client";
 
 export const meta: ArticleMeta = {
   title: "AI user interface patterns",
   blurb:
-    "AI-enabled capabilities can be surfaced in user interfaces in a variety of ways. I want to review those that I've encountered.",
+    "AI-enabled capabilities can be surfaced in user interfaces in a variety of ways. Here's a review of those that I've encountered.",
   shortBlurb: "How might we surface AI capabilities in user interfaces?",
   mainImage: "main.jpg",
   date: "2024-09-13",
@@ -11,5 +11,11 @@ export const meta: ArticleMeta = {
   type: "article",
   tags: ["design"],
   socialLinks: [],
+  discussionLink: {
+    type: SocialLinkTypes.LinkedIn,
+    url: "https://www.linkedin.com/pulse/surfacing-ai-user-interface-jonathan-conway-kyamc/?trackingId=GnKV9%2FnmRD2yS551gV0Y0A%3D%3D",
+    commentCount: 1,
+    likeCount: 1,
+  },
   isPinned: true,
 };
