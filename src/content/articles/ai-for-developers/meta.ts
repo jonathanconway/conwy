4537c8194deda7389c58b6b73380d6f632ec998d
Jonathan Conway <jon@conwy.co>
2024-12-14 14:19:57 +1100

    feat(article-ai-for-developers)

---
+++ src/content/articles/ai-for-developers/meta.ts
@@ -0,0 +1,14 @@
+import { ArticleMeta } from "@/framework";
+
+export const meta: ArticleMeta = {
+  title: "AI for developers",
+  blurb: "How I've been using AI as a developer",
+  shortBlurb: "How I've been using AI as a developer",
+  date: "2024-12-14",
+  slug: "ai-for-developers",
+  type: "article",
+  tags: ["software-development"],
+  mainImage: "main.jpg",
+  socialLinks: [],
+  discussionLinks: [],
+};
