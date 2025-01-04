b6e31fa7c70b1ff576fb0d849f7080c63cc0ed78
Jonathan Conway <jon@conwy.co>
2025-01-05 00:30:04 +1100

    fix(google-tag-manager)

--- src/components/scripts/google-tag-manager/google-tag-manager.tsx
+++ src/components/scripts/google-tag-manager/google-tag-manager.tsx
@@ -1,5 +1,22 @@
-import { GoogleTagManager as NextGoogleTagManager } from "@next/third-parties/google";
+import Head from "next/head";
 
 export function GoogleTagManager() {
-  return <NextGoogleTagManager gtmId="G-25G3CM3VLS" />;
+  return (
+    <Head key="gtm">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-25G3CM3VLS"
+      ></script>
+      <script>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', 'G-25G3CM3VLS');
+      `}
+      </script>
+    </Head>
+  );
 }
