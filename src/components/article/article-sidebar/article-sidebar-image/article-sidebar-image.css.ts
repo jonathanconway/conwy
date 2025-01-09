f41e8e3a2a588e2d5ac8aa9fcd0fec2c2de63e36
Jonathan Conway <jon@conwy.co>
2025-01-09 22:42:51 +1100

    fix(components/article-sidebar-image): add border

--- src/components/article/article-sidebar/article-sidebar-image/article-sidebar-image.css.ts
+++ src/components/article/article-sidebar/article-sidebar-image/article-sidebar-image.css.ts
@@ -1,9 +1,11 @@
 import { style } from "@vanilla-extract/css";
 
+import * as boxMixins from "../../../box/box.mixins";
 import { rounded } from "../../../styling";
 
 export const image = style({
   ...rounded.md,
+  ...boxMixins.boxBorderDecorative,
   width: "80%",
   height: "auto",
 });
