bcedf2e96d172f4508a465ae42e13fa0e257f946
Jonathan Conway <jon@conwy.co>
2025-01-12 22:19:36 +1100

    feat(google-tag-manager): fix to work with react

--- src/components/scripts/google-tag-manager/google-tag-manager.tsx
+++ src/components/scripts/google-tag-manager/google-tag-manager.tsx
@@ -1,4 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
 export function GoogleTagManager() {
+  useEffect(() => {
+    const w = window as any;
+    w.dataLayer = w.dataLayer || [];
+    function gtag(...args: any) {
+      w.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-25G3CM3VLS");
+  }, []);
+
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
@@ -6,15 +21,6 @@ export function GoogleTagManager() {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-25G3CM3VLS"
       ></script>
-      <script>
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      
-      gtag('config', 'G-25G3CM3VLS');
-      `}
-      </script>
     </>
   );
 }
