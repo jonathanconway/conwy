e3642cbd1bf6139d27dd5e87b10974c38c38433a
Jonathan Conway <jon@conwy.co>
2025-03-16 12:30:10 +1100

    content(books/algorithms): add

---
+++ src/content/books/algorithms-louridas.ts
@@ -0,0 +1,9 @@
+import { Book, BookCategories, BookStatuses } from "@/framework/client";
+
+export const algorithmsLouridasBook: Book = {
+  authors: ["Panos Louridas"],
+  status: BookStatuses.Finished,
+  title: "Algorithms",
+  url: "https://direct.mit.edu/books/book/4869/Algorithms",
+  category: BookCategories.SoftwareDevelopment,
+};
