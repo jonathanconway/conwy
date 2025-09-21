485931c35d3d2e0ed7944a36ef772bc398c2f4cd
Jonathan Conway <jon@conwy.co>
2025-09-21 19:00:37 +1000

    content(books): update statuses

--- src/content/books/how-to-eat.ts
+++ src/content/books/how-to-eat.ts
@@ -5,7 +5,7 @@ export const howToEatBook: Book = {
     type: "book",
     slug: "how-to-eat",
     authors: ["Thích Nhất Hạnh"],
-    status: BookStatuses.Reading,
+    status: BookStatuses.Finished,
     title: "How to Eat",
     url: "https://www.penguinrandomhouse.com/books/545835/how-to-eat-by-thich-nhat-hanh",
     category: BookCategories.SelfHelp,
--- src/content/books/mental-toughness.ts
+++ src/content/books/mental-toughness.ts
@@ -5,7 +5,7 @@ export const mentalToughnessBook: Book = {
     type: "book",
     slug: "mental-toughness",
     authors: ["Steve Siebold"],
-    status: BookStatuses.Reading,
+    status: BookStatuses.Finished,
     title: "177 Mental Toughness Secrets",
     url: "https://www.goodreads.com/book/show/2297678.177_Mental_Toughness_Secrets_of_the_World_Class",
     category: BookCategories.SelfHelp,
