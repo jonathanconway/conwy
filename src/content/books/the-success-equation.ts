20b66074ecb190ff447470f7127d5d9a2ee9db46
Jonathan Conway <jon@conwy.co>
2025-12-13 16:36:53 +1100

    content(book): the success equation

---
+++ src/content/books/the-success-equation.ts
@@ -0,0 +1,13 @@
+import { Book, BookCategories, BookStatuses } from "@/framework/content/book";
+
+export const theSuccessEquationBook: Book = {
+  meta: {
+    type: "book",
+    slug: "the-success-equation",
+    authors: ["Michael Mauboussin"],
+    status: BookStatuses.Reading,
+    title: "The Success Equation",
+    url: "https://en.wikipedia.org/wiki/Michael_J._Mauboussin",
+    category: BookCategories.Business,
+  },
+};
