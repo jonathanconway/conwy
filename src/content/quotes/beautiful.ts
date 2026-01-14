dbed4420ee075b9d4071baf85edbdefcf5ed186a
Jonathan Conway <jon@conwy.co>
2026-01-14 14:58:43 +1100

    fix(content/author): name formatting

--- src/content/quotes/beautiful.ts
+++ src/content/quotes/beautiful.ts
@@ -5,7 +5,7 @@ export const beautifulQuote: Quote = {
   text: "I want to learn more and more to see as beautiful what is necessary in things; then I shall be one of those who make things beautiful.",
   meta: {
     author: {
-      title: "Friedrich NIETZSCHE",
+      title: "Friedrich Nietzsche",
       url: "https://en.wikipedia.org/wiki/Friedrich_Nietzsche",
     },
     slug: "beautiful",
