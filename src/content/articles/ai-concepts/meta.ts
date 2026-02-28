5dcf7eac6e750b207b7fc6c99851e13471e7cd22
Jonathan Conway <jon@conwy.co>
2026-02-28 16:12:37 +1100

    fix(content/articles/ai-concepts): blurb

--- src/content/articles/ai-concepts/meta.ts
+++ src/content/articles/ai-concepts/meta.ts
@@ -3,9 +3,9 @@ import { ArticleMeta, PostTags } from "@/framework/client";
 export const meta: ArticleMeta = {
   title: "AI concepts for developers",
   blurb:
-    "Defining core concepts in developer-focussed AI assistants and tooling: Prompts, Commands, Assistants, Models, Agents, Sub Agents and Skills",
+    "Defining core concepts on which AI tooling is based, to gain deeper insight and think more strategically about how best to use AI tools.",
   shortBlurb:
-    "Defining core concepts in developer-focussed AI assistants and tooling: Prompts, Commands, Assistants, Models, Agents, Sub Agents and Skills",
+    "Defining core concepts on which AI tooling is based, to gain deeper insight and think more strategically about how best to use AI tools.",
   date: "2026-02-18",
   slug: "ai-concepts",
   type: "article",
