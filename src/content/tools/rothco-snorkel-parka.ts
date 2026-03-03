6a35f50b71f13c79efe5cabd09933bed930e43b6
Jonathan Conway <jon@conwy.co>
2026-03-03 14:49:22 +1100

    content(tools): add boots, parka, coffee mug, pen

---
+++ src/content/tools/rothco-snorkel-parka.ts
@@ -0,0 +1,16 @@
+import { Tool, ToolSections } from "@/framework/client";
+
+export const rothcoSnorkelParkaTool: Tool = {
+  type: "tool",
+  meta: {
+    slug: "rothco-snorkel-parka",
+    title: "Rothco N3B Snorkel Parka",
+    section: ToolSections.Belonging,
+    category: "Clothing",
+    date: "2026-03-03",
+    mainImage: "/images/tools/rothco-snorkel-parka.webp",
+    operatingSystems: [],
+    description: `Extremely warm, waterproof, comfortable and durable jacket, with a refined and understated style. Great for winter hikes and long hours in offices with freezing air conditioning.`,
+    url: "https://www.rothco.com/product/rothco-n-3b-parka?color=Black",
+  },
+};
---
+++ src/content/tools/zhenmi-coffee-mug.ts
@@ -0,0 +1,16 @@
+import { Tool, ToolSections } from "@/framework/client";
+
+export const zhenmiCoffeeMugTool: Tool = {
+  type: "tool",
+  meta: {
+    slug: "zhenmi-coffee-mug",
+    title: "Zhenmi Electric Water Cup",
+    section: ToolSections.Belonging,
+    category: "Mug",
+    date: "2026-03-02",
+    mainImage: "/images/tools/zhenmi-coffee-mug.avif",
+    operatingSystems: [],
+    description: `Sleek, elegant, durable coffee mug. Screw-on tight-sealing lid with sealable mouth piece. USB-C powered kettle function, great for self-made instant coffee at the airport.`,
+    url: "https://www.aliexpress.com/item/1005008519406964.html",
+  },
+};
