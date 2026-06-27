ffe315028447c68e23d1295a08c3f333924ae96d
Jonathan Conway <jonathan.conway@gmail.com>
2026-06-27 20:13:05 +1000

    content(tools): oasis-bottle

---
+++ src/content/tools/oasis-bottle/meta.ts
@@ -0,0 +1,13 @@
+import { ToolMeta, ToolSections } from "@/framework/client";
+
+export const meta: ToolMeta = {
+  slug: "oasis-bottle",
+  title: "Oasis Insulated Stainless Steel Skinny Mini Drink Bottle",
+  section: ToolSections.Belonging,
+  category: "Mug",
+  date: "2026-06-27",
+  mainImage: "/images/tools/oasis-bottle.jpg",
+  operatingSystems: [],
+  description: `Sleek, slim, elegant, durable coffee mug. Screw-on tight-sealing lid.`,
+  url: "https://www.hsw.com.au/oasis-insulated-stainless-steel-skinny-mini-drink-bottle-250ml-black/",
+};
