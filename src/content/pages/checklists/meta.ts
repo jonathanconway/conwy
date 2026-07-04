03d1fa699560657463152785e612f66455c09b8a
Jonathan Conway <jonathan.conway@gmail.com>
2026-07-04 20:31:33 +1000

    fix(checklists): remove legacy checklists page

--- src/content/pages/checklists/meta.ts
+++
@@ -1,7 +0,0 @@
-import { PageMeta } from "@/framework/client";
-
-export const meta: PageMeta = {
-  title: "Checklists",
-  slug: "checklists",
-  type: "page",
-};
--- src/content/pages/index.ts
+++ src/content/pages/index.ts
@@ -1,5 +1,4 @@
 export * from "./about-me";
-export * from "./checklists";
 export * from "./listening";
 export * from "./speaking";
 export * from "./follows";
