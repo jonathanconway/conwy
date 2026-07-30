de058306e071504f1597780d8f5c0b4e5feeb664
Jonathan Conway <jonathan.conway@gmail.com>
2026-07-30 11:14:46 +1000

    content(quotes/leadership)

--- src/content/quotes/index.ts
+++ src/content/quotes/index.ts
@@ -14,6 +14,7 @@ export * from "./habit";
 export * from "./investigate";
 export * from "./judgements";
 export * from "./legacy";
+export * from "./leadership";
 export * from "./nature-questioning";
 export * from "./not-enough-time";
 export * from "./perfection";
---
+++ src/content/quotes/leadership.ts
@@ -0,0 +1,12 @@
+import { Quote } from "@/framework/client";
+
+export const leadershipQuote: Quote = {
+  type: "quote",
+  text: "Leadership is the art of getting someone else to do something you want done because they want to do it.",
+  meta: {
+    author: {
+      title: "Dwight Eisenhower",
+    },
+    slug: "leadership",
+  },
+};
