1e6955c6607a6db12f030e0384971fc79aaf36ea
Jonathan Conway <jonathan.conway@gmail.com>
2026-08-08 22:50:34 +1000

    content(quotes/idiot)

---
+++ src/content/quotes/idiot.ts
@@ -0,0 +1,16 @@
+import { Quote } from "@/framework/client";
+
+export const idiotQuote: Quote = {
+  type: "quote",
+  text: "I always hope that I can look back every year on myself the previous year and feel like that guy last year was an idiot.",
+  meta: {
+    author: {
+      title: "Benjamin Felix",
+      url: "https://pwlcapital.com/our-team/benjamin-felix/",
+    },
+    source: {
+      url: "https://www.youtube.com/watch?v=2uJq7OQhpGg",
+    },
+    slug: "idiot",
+  },
+};
--- src/content/quotes/index.ts
+++ src/content/quotes/index.ts
@@ -13,6 +13,7 @@ export * from "./good-example";
 export * from "./good-old-days";
 export * from "./habit";
 export * from "./investigate";
+export * from "./idiot";
 export * from "./judgements";
 export * from "./knowledge";
 export * from "./leadership";
