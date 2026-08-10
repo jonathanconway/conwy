837f6639ae49a29e1024aff1754941bd1c11f902
Jonathan Conway <jonathan.conway@gmail.com>
2026-08-10 14:25:54 +1000

    content(books): flashpoints, engineering-after-the-vibe-shift

---
+++ src/content/books/flashpoints/meta.ts
@@ -0,0 +1,15 @@
+import {
+  BookCategories,
+  BookMeta,
+  BookStatuses,
+} from "@/framework/content/book";
+
+export const meta: BookMeta = {
+  type: "book",
+  slug: "flashpoints",
+  authors: ["George Friedman"],
+  status: BookStatuses.Finished,
+  title: "Flashpoints: The Emerging Crisis in Europe",
+  url: "https://en.wikipedia.org/wiki/Flashpoints_(book)",
+  category: BookCategories.History,
+};
---
+++ src/content/books/software-engineering-after-the-vibe-shift/meta.ts
@@ -0,0 +1,15 @@
+import {
+  BookCategories,
+  BookMeta,
+  BookStatuses,
+} from "@/framework/content/book";
+
+export const meta: BookMeta = {
+  type: "book",
+  slug: "software-engineering-after-the-vibe-shift",
+  authors: ["Sean Goedecke"],
+  status: BookStatuses.Reading,
+  title: "Software Engineering after the Vibe Shift",
+  url: "https://www.seangoedecke.com/book",
+  category: BookCategories.SoftwareEngineering,
+};
