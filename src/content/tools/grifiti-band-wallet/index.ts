f2d280e7df595eb67c1857132fc76f428c61ad30
Jonathan Conway <jonathan.conway@gmail.com>
2026-08-25 15:24:14 +1000

    fix(content/tools/grifiti-band-waller): move into a folder

---
+++ src/content/tools/grifiti-band-wallet/index.ts
@@ -0,0 +1,5 @@
+import { Tool } from "@/framework/client";
+
+import { meta } from "./meta";
+
+export const grifitiBandWalletTool: Tool = { type: "tool", meta };
---
+++ src/content/tools/grifiti-band-wallet/meta.ts
@@ -0,0 +1,13 @@
+import { ToolMeta, ToolSections } from "@/framework/client";
+
+export const meta: ToolMeta = {
+  slug: "grifiti-band-wallet",
+  title: "Grifiti Band Wallet",
+  section: ToolSections.Belonging,
+  date: "2026-07-26",
+  mainImage: "/images/tools/grifiti-band-wallet.jpg",
+  description:
+    "Minimalism at its purest! A simple rubber band. Sleek but strong and perfectly sized for a small bundle of credit cards. Treated with care, they last for years.",
+  operatingSystems: [],
+  url: "https://grifiti.com/products/grifiti-band-joes-3-25-x-1-25-in-standard-bands-5pk?variant=31943394197613",
+};
