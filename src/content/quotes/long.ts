5a45ccc8b71c8af2dacc87c7ee60bd3c9977ed6a
Jonathan Conway <jonathan.conway@gmail.com>
2026-09-06 23:51:25 +1000

    content(quotes): long, valuable

---
+++ src/content/quotes/long.ts
@@ -0,0 +1,12 @@
+import { Quote } from "@/framework/client";
+
+export const longQuote: Quote = {
+  type: "quote",
+  text: "The long term is made up of many short terms.",
+  meta: {
+    author: {
+      title: "Ronnie Coleman",
+    },
+    slug: "long",
+  },
+};
---
+++ src/content/quotes/valuable.ts
@@ -0,0 +1,12 @@
+import { Quote } from "@/framework/client";
+
+export const valuableQuote: Quote = {
+  type: "quote",
+  text: "Strive not to be a success, but rather to be of value.",
+  meta: {
+    author: {
+      title: "Albert Einstein",
+    },
+    slug: "valuable",
+  },
+};
