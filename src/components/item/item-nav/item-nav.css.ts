af1f47da54d6365282f05d690d72c19fa61af47e
Jonathan Conway <jonathan.conway@gmail.com>
2026-09-18 09:33:47 +1000

    refactor(components/item-nav): mixins

--- src/components/item/item-nav/item-nav.css.ts
+++ src/components/item/item-nav/item-nav.css.ts
@@ -1,70 +1,22 @@
 import { globalStyle, style } from "@vanilla-extract/css";
 
-import * as labelMixins from "../../label/label.mixins";
-import { gap, media, responsive, rounded, size, text } from "../../styling";
-import { vars } from "../../theme";
+import * as mixins from "./item-nav.mixins";
 
-export const navsContainer = style({
-  display: "flex",
-  flex: 1,
-  alignItems: "center",
-  ...responsive.flexDirectionMobileColumnRowElse,
-  ...gap._2,
-});
+export const navsContainer = style(mixins.navsContainer);
 
 globalStyle(`${navsContainer} > *`, {
   flex: 1,
   width: "100%",
 });
 
-export const containerEmpty = style({
-  display: "flex",
-  width: size.full,
-  ...rounded.md,
-  padding: "0.25rem",
-  backgroundColor: vars.itemNav.placeholder.background.color,
-  opacity: 0.5,
+export const containerEmpty = style(mixins.containerEmpty);
 
-  "@media": {
-    [media.sm]: {
-      display: "none",
-    },
-  },
-});
+export const mainColumn = style(mixins.mainColumn);
 
-export const mainColumn = style({
-  display: "flex",
-  flexDirection: "column",
-  flex: 1,
-  gap: "0.25rem",
-  justifyContent: "center",
-  overflow: "hidden",
-});
+export const asideColumn = style(mixins.asideColumn);
 
-export const asideColumn = style({
-  display: "flex",
-  alignContent: "center",
-});
+export const label = style(mixins.label);
 
-export const label = style({
-  ...labelMixins.label,
-  display: "flex",
-  alignItems: "center",
-  gap: "0.25rem",
-});
+export const labelText = style(mixins.labelText);
 
-export const labelText = style({});
-
-export const title = style({
-  fontWeight: "bold",
-  ...text.size.xs,
-  textOverflow: "ellipsis",
-  overflow: "hidden",
-  whiteSpace: "nowrap",
-
-  "@media": {
-    [media.sm]: {
-      maxWidth: "20vw",
-    },
-  },
-});
+export const title = style(mixins.title);
---
+++ src/components/item/item-nav/item-nav.mixins.ts
@@ -0,0 +1,70 @@
+import { ComplexStyleRule } from "@vanilla-extract/css";
+
+import * as labelMixins from "../../label/label.mixins";
+import { gap, media, responsive, rounded, size, text } from "../../styling";
+import { vars } from "../../theme";
+
+export const navsContainer = {
+  display: "flex",
+  flex: 1,
+  alignItems: "center",
+  ...responsive.flexDirectionMobileColumnRowElse,
+  ...gap._2,
+};
+
+export const navItem = {
+  flex: 1,
+  width: "100%",
+};
+
+export const containerEmpty = {
+  display: "flex",
+  width: size.full,
+  ...rounded.md,
+  padding: "0.25rem",
+  backgroundColor: vars.itemNav.placeholder.background.color,
+  opacity: 0.5,
+
+  "@media": {
+    [media.sm]: {
+      display: "none",
+    },
+  },
+};
+
+export const mainColumn: ComplexStyleRule = {
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  gap: "0.25rem",
+  justifyContent: "center",
+  overflow: "hidden",
+};
+
+export const asideColumn = {
+  display: "flex",
+  alignContent: "center",
+};
+
+export const label = {
+  ...labelMixins.label,
+  display: "flex",
+  alignItems: "center",
+  gap: "0.25rem",
+};
+
+export const labelText = {};
+
+export const title = {
+  fontWeight: "bold",
+  ...text.size.xs,
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+
+  "@media": {
+    [media.sm]: {
+      maxWidth: "20vw",
+    },
+  },
+};
